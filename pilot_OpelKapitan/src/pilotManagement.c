43984569b47f8026a1811cb62b9bd7cb9df08133
Tangui <tangui.steimetz@ecole.ensicaen.fr>
2022-04-12 16:33:02 +0200

    retire commentaire

--- pilot_OpelKapitan/src/pilotManagement.c
+++ pilot_OpelKapitan/src/pilotManagement.c
@@ -25,27 +25,18 @@
 #define STRAIGHT_ACTION "straight"
 #define DEFAULT_ACTION "default"
 #define NEW_ACTION "new"
+#define DONT_CARE_ACTION 0
 #define ACTION_IS_UNCHANGED(choice) strcmp ( choice, UNCHANGED_ACTION ) == 0 ? true : false
 #define ACTION_IS_STRAIGHT(choice) strcmp ( choice, STRAIGHT_ACTION ) == 0 ? true : false
 #define ACTION_IS_DEFAULT(choice) strcmp ( choice, DEFAULT_ACTION ) == 0 ? true : false
+/*
+A changer probablement
+*/
 #define X "x"
 #define Y "y"
 #define VALUE_DEFAULT_ACTION(i) ( strcmp ( i, X ) == 0 ) ? 1 : 0
-#define DONT_CARE_ACTION 0
-
-/*
-static void putXPositionPilot ( PILOT* pilot, short newX );
-
-static void putYPositionPilot ( PILOT* pilot, short newY );
-*/
 
 static void putPositionPilot ( PILOT* pilot, short xPosition, short yPosition );
-
-/*
-static void putXSpeedPilot ( PILOT* pilot, short newXSpeed );
-
-static void putYSpeedPilot ( PILOT* pilot, short neuYSpeed );
-*/
 
 static void putSpeedPilot ( PILOT* pilot, short xSpeed, short ySpeed );
 
@@ -54,8 +45,6 @@ static void putGasLvlPilot ( PILOT* pilot, short newGasLvl );
 static void putActionPilot ( PILOT* pilot, short newXAcc, short newYAcc );
 
 static void initNewPilot ( PILOT* pilot );
-
-/* static void instantSpeed ( short newXPosition, short newYPosition, short previousXPosition, short previousYPosition, short* xSpeed, short* ySpeed ); */
 
 static void updatePositionPilot ( PILOT* myPilot, PILOT* secondPilot, PILOT* thirdPilot );
 
@@ -68,18 +57,6 @@ static short fuelConsumption ( short xSpeed, short ySpeed, short newXAcc, short 
 static void updateGasPilot ( PILOT* pilot );
 
 static void deliverAction ( char* action ); /* action du type : x y */
-
-/*
-static void putXPositionPilot ( PILOT* pilot, short newX )
-{
-    pilot->xPosition = newX;
-}
-
-static void putYPositionPilot ( PILOT* pilot, short newY )
-{
-    pilot->yPosition = newY;
-}
-*/
 
 static void putPositionPilot ( PILOT* pilot, short xPosition, short yPosition )
 {
@@ -113,19 +90,6 @@ static void initNewPilot ( PILOT* pilot )
     putSpeedPilot ( pilot, 0, 0 );
     putActionPilot ( pilot, VALUE_DEFAULT_ACTION(X), VALUE_DEFAULT_ACTION(Y) );
 }
-
-/*
-static void instantSpeed ( short newXPosition, short newYPosition, short previousXPosition, short previousYPosition, short* xSpeed, short* ySpeed )
-{
-    DEBUG_CHAR ( "===> Calcul de la vitesse = ", ' ' );
-    DEBUG_INT ( "Valeur nouvelle position x = ", (int) newXPosition );
-    DEBUG_INT ( "Valeur ancienne position x = ", (int) previousXPosition );
-    DEBUG_INT ( "Valeur nouvelle position y = ", (int) newYPosition );
-    DEBUG_INT ( "Valeur ancienne position y = ", (int) previousYPosition );
-    *xSpeed = newXPosition - previousXPosition;
-    *ySpeed = newYPosition - previousYPosition;
-}
-*/
 
 static void updateSpeedPilot ( PILOT* pilot )
 {
@@ -201,27 +165,6 @@ static void updateGasPilot ( PILOT* pilot )
                     );
     DEBUG_INT ( "> gas left : ", getGasLvlPilot ( pilot ) );
 }
-/*
-static void updatePilot ( PILOT* pilot, short newX, short newY, short newGasLvl, short newXAcc, short newYAcc, char* modeChosen )
-{
-    putXPositionPilot ( pilot, newX );
-    putYPositionPilot ( pilot, newY );
-    putGasLvlPilot ( pilot, newGasLvl );
-
-    if ( ACTION_IS_UNCHANGED ( modeChosen ) ){
-        DEBUG_STRING ( "====> update action : ", "ne rien faire" );
-    } else if ( ACTION_IS_STRAIGHT ( modeChosen ) ) {
-        DEBUG_STRING ( "====> update action : ", "ne pas changer sa direction, ni son acc" );
-        putActionPilot ( pilot, (short) 0, (short) 0 );
-    } else if ( ACTION_IS_DEFAULT ( modeChosen ) ) {
-        DEBUG_STRING ( "====> update action : ", "en mode default (1 0)" );
-        putActionPilot ( pilot, (short) 1, (short) 0 );
-    } else {
-        DEBUG_STRING ( "====> update action : ", "Changement d'acc" );
-        putActionPilot ( pilot, newXAcc, newYAcc );
-    }
-}
-*/
 
 static void deliverAction ( char action[SIZE_ACTION] )
 {
@@ -309,65 +252,3 @@ void updatePilots ( PILOT* myPilot, PILOT* secondPilot, PILOT* thirdPilot )
     sprintf ( action, "%hd %hd", getXAccPilot ( myPilot ), getYAccPilot ( myPilot ) );
     deliverAction ( action );
 }
-
-/*
-void readPilotsPosition ( PILOT* myPilot, PILOT* secondPilot, PILOT* thirdPilot )
-{
-    char buf[MAX_LINE_LENGTH];
-    short myXPosition, myYPosition;
-    short secondXPosition, secondYPosition;
-    short thirdXPosition, thirdYPosition;
-    short myXSpeed, myYSpeed;
-    short xAcc, yAcc;
-    int gasConsumption;
-    static short round = 0;
-
-    round++;
-
-    fgets ( buf, MAX_LINE_LENGTH, stdin );
-    fflush ( stdin );
-    sscanf ( buf, "%hd %hd %hd %hd %hd %hd",
-                    &myXPosition, &myYPosition,
-                    &secondXPosition, &secondYPosition,
-                    &thirdXPosition, &thirdYPosition
-            );
-
-    xAcc = getXAccPilot ( myPilot );
-    yAcc = getYAccPilot ( myPilot );
-    
-    DEBUG_CHAR ( "====> lecture Position : ", ' ' );
-    DEBUG_INT ( "Position mon pilot : x = ", (int) myXPosition );
-    DEBUG_INT ( "Position mon pilot : y = ", (int) myYPosition );
-    DEBUG_INT ( "Valeur acc x = ", (int) xAcc );
-    DEBUG_INT ( "Valeur acc y = ", (int) yAcc );
-
-    if ( round == 1 ) {
-        instantSpeed ( myXPosition, myYPosition, myXPosition, myYPosition, &myXSpeed, &myYSpeed );
-    } else {
-        instantSpeed ( myXPosition, myYPosition, getXPositionPilot ( myPilot ), getYPositionPilot ( myPilot ), &myXSpeed, &myYSpeed );
-    }
-
-    gasConsumption = fuelConsumption ( myXSpeed, myYSpeed, xAcc, yAcc );
-
-    DEBUG_CHAR ( "====> Calcul vitesse et fuel : ", ' ' );
-    DEBUG_INT ( "Vitesse mon pilot : x = ", (int) myXSpeed );
-    DEBUG_INT ( "Vitesse mon pilot : y = ", (int) myYSpeed );
-    DEBUG_INT ( "Delta Fuel mon pilot = ", (int) gasConsumption );
-
-    if ( round == 1 ) {
-        updatePilot ( myPilot, myXPosition, myYPosition, getGasLvlPilot ( myPilot ) - gasConsumption, 0, 0, DEFAULT_ACTION );
-    } else if ( round == 2 ) {
-        updatePilot ( myPilot, myXPosition, myYPosition, getGasLvlPilot ( myPilot ) - gasConsumption, 0, 0, UNCHANGED_ACTION );
-    } else if ( round == 4 ) {
-        updatePilot ( myPilot, myXPosition, myYPosition, getGasLvlPilot ( myPilot ) - gasConsumption, 2, 0, NEW_ACTION );
-    } else {
-        updatePilot ( myPilot, myXPosition, myYPosition, getGasLvlPilot ( myPilot ) - gasConsumption, 2, 0, STRAIGHT_ACTION );
-    }
-    */
-    /* Inutile le temps de debug, les msg c'est chiant;
-    updatePilot ( secondPilot, secondXPosition, secondYPosition, 0, 0, 0, UNCHANGED_ACTION );
-    updatePilot ( thirdPilot, thirdXPosition, thirdYPosition, 0, 0, 0, UNCHANGED_ACTION );
-    */
-/*
-}
-*/
