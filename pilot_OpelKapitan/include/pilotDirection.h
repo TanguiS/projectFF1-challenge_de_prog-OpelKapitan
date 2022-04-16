a7ef9afaa10e84cbdafd9156e3545c5ccb0354ad
kylian <kylian.picque@ecole.ensicaen.fr>
2022-04-16 10:44:07 +0200

    premier fonctions

---
+++ pilot_OpelKapitan/include/pilotDirection.h
@@ -0,0 +1,128 @@
+/**
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */
+
+/**
+ * @file pilotDirection.h
+ * @brief This file contains the prototypes / declarations used to choose the pilot's direction.
+ */
+
+/**
+ * @author PICQUE Kylian <kylian.picque@ecole.ensicaen.fr>
+ * @version 1.0.0
+ * @date 15 avril 2022
+ */
+
+
+
+#ifndef __PILOT_DIERCTION__
+#define __PILOT_DIRECTION__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "pilotManagement.h"
+
+/**
+ * @brief turn the pilot on the right the next round
+ * 
+ * @param pilot the pilot 
+ */ 
+void goRight(PILOT* pilot); 
+
+/**
+ * @brief turn the pilot on the left the next round
+ * 
+ * @param pilot the pilot 
+ */ 
+void goLeft(PILOT* pilot);
+
+/**
+ * @brief turn up the pilot the next round
+ * 
+ * @param pilot the pilot 
+ */ 
+void goUp(PILOT* pilot);
+
+/**
+ * @brief turn down the pilot the next round
+ * 
+ * @param pilot the pilot 
+ */ 
+void goDown(PILOT* pilot);
+
+/**
+ * @brief stop the pilot the next round
+ * 
+ * @param pilot the pilot 
+ */ 
+void stop(PILOT* pilot);
+
+/**
+ * @brief turn on the diagonal right up
+ * 
+ * @param pilot the pilot 
+ */ 
+void goRightUp(PILOT* pilot);
+
+/**
+ * @brief turn on the diagonal left up
+ * 
+ * @param pilot the pilot 
+ */ 
+void goLeftUp(PILOT* pilot);
+
+/**
+ * @brief turn on the diagonal right down
+ * 
+ * @param pilot the pilot 
+ */ 
+void goRightDown(PILOT* pilot);
+
+/**
+ * @brief turn on the diagonal left down
+ * 
+ * @param pilot the pilot 
+ */ 
+void goLeftDown(PILOT* pilot);
+
+
+void boostRight(PILOT* pilot);
+
+void boostLeft(PILOT* pilot);
+
+void boostUp(PILOT* pilot);
+
+void boostDown(PILOT* pilot);
+
+void boostRRightUUp(PILOT* pilot);
+
+void boostLLeftUUp(PILOT* pilot);
+
+void boostRRightDDown(PILOT* pilot);
+
+void boostLLeftDDown(PILOT* pilot);
+
+void boostLeftDDown(PILOT* pilot);
+
+void boostLLeftDown(PILOT* pilot);
+
+void boostRightDDown(PILOT* pilot);
+
+void boostRRightDown(PILOT* pilot);
+
+void boostLeftUUp(PILOT* pilot);
+
+void boostLLeftUp(PILOT* pilot);
+
+void boostRightUUp(PILOT* pilot);
+
+
+
+
+#endif
---
+++ pilot_OpelKapitan/src/pilotDirection.c
@@ -0,0 +1,167 @@
+/**
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */
+
+/**
+ * @file pilotDirection.c
+ * @brief This file contains the functions used to choose the pilot's direction .
+ */
+
+/**
+ * @author PICQUE Kylian <kylian.picque@ecole.ensicaen.fr>
+ * @version 1.0.0
+ * @date 15 avril 2022
+ */
+
+
+#include "pilotDirection.h"
+
+
+
+void goRight ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +1;
+    pilot->yAcc = -pilot->ySpeed;
+}
+
+
+void goLeft ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed -1;
+    pilot->yAcc = -pilot->ySpeed;
+}
+
+
+
+void goUp ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed;
+    pilot->yAcc = -pilot->ySpeed + 1;
+}
+
+
+void goDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed;
+    pilot->yAcc = -pilot->ySpeed - 1;
+}
+
+
+void stop ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed;
+    pilot->yAcc = -pilot->ySpeed;
+}
+
+
+void goRightUp ( PILOT* pilot ) { 
+    pilot->xAcc = -pilot->xSpeed +1;
+    pilot->yAcc = -pilot->ySpeed + 1;
+}
+
+
+void goLeftUp ( PILOT* pilot ) {  
+    pilot->xAcc = -pilot->xSpeed -1;
+    pilot->yAcc = -pilot->ySpeed + 1;
+}
+
+
+void goRightDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +1;
+    pilot->yAcc = -pilot->ySpeed - 1;
+}
+
+
+void goLeftDown ( PILOT* pilot ) { 
+    pilot->xAcc = -pilot->xSpeed -1;
+    pilot->yAcc = -pilot->ySpeed - 1;
+}
+
+
+void boostRight ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +2;
+    pilot->yAcc = -pilot->ySpeed;
+}
+
+void boostLeft ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed -2;
+    pilot->yAcc = -pilot->ySpeed;
+}
+
+
+void boostUp ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed;
+    pilot->yAcc = -pilot->ySpeed + 2;
+}
+
+
+void boostDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed;
+    pilot->yAcc = -pilot->ySpeed - 2;
+}
+
+
+void boostRRightUUp ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +2;
+    pilot->yAcc = -pilot->ySpeed +2;   
+}
+
+
+void boostLLeftUUp ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed -2;
+    pilot->yAcc = -pilot->ySpeed +2;   
+}
+
+
+void boostRRightDDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +2;
+    pilot->yAcc = -pilot->ySpeed -2;   
+}
+
+
+void boostLLeftDDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed -2;
+    pilot->yAcc = -pilot->ySpeed -2;   
+}
+
+
+void boostLeftDDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed -1;
+    pilot->yAcc = -pilot->ySpeed -2;
+}
+
+void boostLLeftDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed -2;
+    pilot->yAcc = -pilot->ySpeed -1;
+}
+
+void boostRightDDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +1;
+    pilot->yAcc = -pilot->ySpeed -2;
+}
+
+void boostRRightDown ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +2;
+    pilot->yAcc = -pilot->ySpeed -1;
+}
+
+void boostLeftUUp ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed -1;
+    pilot->yAcc = -pilot->ySpeed +2;
+} 
+
+void boostLLeftUp ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed -2;
+    pilot->yAcc = -pilot->ySpeed +1;
+}
+
+void boostRightUUp ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +1;
+    pilot->yAcc = -pilot->ySpeed +2;
+}
+
+void boostRRightUp ( PILOT* pilot ) {
+    pilot->xAcc = -pilot->xSpeed +2;
+    pilot->yAcc = -pilot->ySpeed +1;
+}
