5f6855963512fe384dc75d2650b813ce1019bced
KylianPi <kylian.picque@ecole.ensicaen.fr>
2022-05-09 22:37:27 +0200

    je recup

--- pilot_OpelKapitan/src/test/main_dijkstra.c
+++ pilot_OpelKapitan/src/test/main_dijkstra.c
@@ -30,11 +30,24 @@
 #define Y 12
 #define X_SMALL 3
 #define Y_SMALL 4
+#define X_MID 7
+#define Y_MID 8
 
 const short smallGraph[X_SMALL][Y_SMALL] = 
-            { { 0, 0, 1, 0 },
+            { { 0, 0, 0, 0 },
               { 1, 1, 1, 1 },
               { 1, 1, 1, 0 },
+            };
+
+const short midGraph[X_MID][Y_MID] = 
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 1, 1, 0, 1, 0, 0 },
+                { 0, 1, 1, 1, 1, 1, 1, 1 },
+                { 0, 1, 1, 0, 0, 1, 1, 1 },
+                { 0, 0, 0, 0, 1, 1, 0, 1 },
+                { 0, 1, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 1, 1, 1 }
             };
 
 const short graphNSand[X][Y] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
@@ -65,27 +78,94 @@ const short coordStart[SIZE_COORD] = { 2, 7 };
 const short coordEnd[SIZE_COORD] = { 6, 7 };
 const short coordStartSmall[SIZE_COORD] = {0, 2};
 const short coordEndSmall[SIZE_COORD] = {3, 1};
+const short coordStartMid[SIZE_COORD] = {1, 2};
+const short coordEndMid[SIZE_COORD] = {1, 5};
 
 int main ( void )
 {
-    int i, j;
-    GRAPH graph = createGraph ( Y_SMALL, X_SMALL );
-    dijkstraMatrix dij = createDijkstraMatrix ( Y_SMALL, X_SMALL );
+    coord path[1000];
+    int count = 0;
+/*     int i, j;
+    GRAPH graph = createGraph ( Y_MID, X_MID );
+    dijkstraMatrix dij = createDijkstraMatrix ( Y_MID, X_MID );
     LIFO stack;
     lifoElement result;
-
-    printf ( "depart value  : %d\n", smallGraph[coordStartSmall[1]][coordStartSmall[0]] );
-    printf ( "end value     : %d\n", smallGraph[coordEndSmall[1]][coordEndSmall[0]] );
-
+    printf ( "depart value  : %d\n", midGraph[coordStartMid[1]][coordStartMid[0]] );
+    printf ( "end value     : %d\n", midGraph[coordEndMid[1]][coordEndMid[0]] );
     for ( i = 0; i < getHeightGraph ( &graph ); i++ ) {
         for ( j = 0; j < getWidthGraph ( &graph ); j++ ) {
-            graph.graph.matrix[j][i] = (element)smallGraph[i][j];
+            graph.graph.matrix[j][i] = (element)midGraph[i][j];
         }
     }
     graph.closestFinishLine[0] = coordEndSmall[0];
     graph.closestFinishLine[1] = coordEndSmall[1];
-    displayDijkstraMatrix ( &dij );
-    stack = givePath ( &dij, &graph, coordStartSmall[0], coordStartSmall[1], coordEndSmall[0], coordEndSmall[1] );
+    displayGraph ( &graph );
+    displayDijkstraMatrix ( &dij, -1, -1 );
+    stack = givePath ( &dij, &graph, coordStartMid[0], coordStartMid[1], coordEndMid[0], coordEndMid[1] );
+    displayDijkstraMatrix ( &dij, -1, -1 );
+    printf ( "\n\n" );
+    while ( !isEmpty ( stack ) ) {
+        stack = removeElementLifo ( stack, &result );
+        printf ( "[%d, %d] ", result[0], result[1] );
+    }
+    printf ( "\n" );
+    destroyDijkstraMatrix ( dij );
+    destroyGraph ( graph ); */
+
+    /**
+     * @brief Gran graph sans sable : 
+     */
+
+/*     int i, j;
+    GRAPH graph = createGraph ( Y, X );
+    dijkstraMatrix dij = createDijkstraMatrix ( Y, X );
+    LIFO stack;
+    lifoElement result;
+    printf ( "depart value  : %d\n", midGraph[coordStart[1]][coordStart[0]] );
+    printf ( "end value     : %d\n", midGraph[coordEnd[1]][coordEnd[0]] );
+    for ( i = 0; i < getHeightGraph ( &graph ); i++ ) {
+        for ( j = 0; j < getWidthGraph ( &graph ); j++ ) {
+            graph.graph.matrix[j][i] = (element)graphNSand[i][j];
+        }
+    }
+    graph.closestFinishLine[0] = coordEndSmall[0];
+    graph.closestFinishLine[1] = coordEndSmall[1];
+    displayGraph ( &graph );
+    displayDijkstraMatrix ( &dij, -1, -1 );
+    stack = givePath ( &dij, &graph, coordStart[0], coordStart[1], coordEnd[0], coordEnd[1] );
+    displayDijkstraMatrix ( &dij, -1, -1 );
+    printf ( "\n\n" );
+    while ( !isEmpty ( stack ) ) {
+        stack = removeElementLifo ( stack, &result );
+        printf ( "[%d, %d] ", result[0], result[1] );
+    }
+    printf ( "\n" );
+    destroyDijkstraMatrix ( dij );
+    destroyGraph ( graph ); */
+
+        /**
+     * @brief Gran graph sans sable : 
+     */
+
+    int i, j;
+    GRAPH graph = createGraph ( Y, X );
+    dijkstraMatrix dij = createDijkstraMatrix ( Y, X );
+    LIFO stack;
+    lifoElement result;
+
+    printf ( "depart value  : %d\n", midGraph[coordStart[1]][coordStart[0]] );
+    printf ( "end value     : %d\n", midGraph[coordEnd[1]][coordEnd[0]] );
+
+    for ( i = 0; i < getHeightGraph ( &graph ); i++ ) {
+        for ( j = 0; j < getWidthGraph ( &graph ); j++ ) {
+            graph.graph.matrix[j][i] = (element)graphWSand[i][j];
+        }
+    }
+    graph.closestFinishLine[0] = coordEndSmall[0];
+    graph.closestFinishLine[1] = coordEndSmall[1];
+    displayGraph ( &graph );
+    displayDijkstraMatrix ( &dij, -1, -1 );
+    stack = givePath ( &dij, &graph, coordStart[0], coordStart[1], coordEnd[0], coordEnd[1] );
 
     displayDijkstraMatrix ( &dij, -1, -1 );
     printf ( "\n\n" );
@@ -93,13 +173,16 @@ int main ( void )
     while ( !isEmpty ( stack ) ) {
         stack = removeElementLifo ( stack, &result );
         printf ( "[%d, %d] ", result[0], result[1] );
+        path[count][0] = result[0];
+        path[count][1] = result[1];
+        count++;
     }
     printf ( "\n" );
+    displayDijkstraMatrixPath ( &dij, count, path );
 
 
     destroyDijkstraMatrix ( dij );
     destroyGraph ( graph );
+
     return EXIT_SUCCESS;
 }
-
-/*désolé tangui*/
