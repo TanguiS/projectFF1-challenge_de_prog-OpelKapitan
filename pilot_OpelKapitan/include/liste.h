897a3717a99493aab8c7f238f7570dcc9fa68770
KylianPi <kylian.picque@ecole.ensicaen.fr>
2022-05-10 20:13:14 +0200

    firstListe

---
+++ pilot_OpelKapitan/include/liste.h
@@ -0,0 +1,105 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */
+
+/**
+ * @file file.h
+ * @brief This file contains all the prototype of the file algo.
+ */
+
+/**
+ * @author STEIMETZ Tangui <steimetz.tangui@ecole.ensicaen.fr>
+ * @version 1.0.1
+ * @date 22 janvier 2022
+ */
+
+#ifndef __FILE_H__
+#define __FILE_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "util.h"
+
+/**
+ * @brief listeElement type declaration 
+ */
+typedef coord listeElement;
+
+/**
+ * @brief A structure to represent a cell object 
+ */
+typedef struct _liste_cell {
+    listeElement contents;           /**< the element that the cell contains */
+    struct _liste_cell* followingCell;    /**< the following cell */
+} _liste_cell; 
+/**
+ * @brief Cell object declaration 
+ */
+typedef struct _liste_cell *LISTE_CELL;
+
+/**
+ * @brief A structure to represent a lifo object 
+ */
+typedef struct _liste {
+    LISTE_CELL head;                      /**< the head of the liste */
+    LISTE_CELL tail;                      /**< the tail of the liste */
+    /*int size;*/                             /**< the size of the liste */
+} _lifo; 
+/**
+ * @brief Lifo object declaration 
+ */
+typedef struct _liste LISTE;
+
+
+/**
+ * @brief Create a Lifo object
+ * 
+ * @return LISTE 
+ */
+LISTE createListe ();
+
+/**
+ * @brief If the lifo is empty
+ * 
+ * @param lifo 
+ * @return boolean 
+ */
+boolean isEmpty ( LISTE lifo );
+
+/**
+ * @brief Add a lifo element int the lifo
+ * 
+ * @param file 
+ * @param x : the element to add
+ * @return LISTE 
+ */
+LISTE addElementLifo ( LISTE file, listeElement x );
+
+
+
+LISTE removeElementListeCoord ( LISTE liste, listeElement* result , coord removeCoord);
+
+
+/**
+ * @brief Remove a lifo element from the lifo
+ * 
+ * @param file 
+ * @param result : the removed element
+ * @return LISTE 
+ */
+LISTE removeElementLifo ( LISTE file, listeElement* result );
+
+/**
+ * @brief Destroy a Lifo Object
+ * 
+ * @param file 
+ */
+void destroyLifo ( LISTE file );
+
+#endif /* __FILE_H__ */
---
+++ pilot_OpelKapitan/src/liste.c
@@ -0,0 +1,145 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin
+ * F-14050 Caen Cedex
+ *
+ * This liste is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */
+
+/**
+ * @liste liste.c
+ * @brief This liste contains all the functions of the liste algo.
+ */
+
+/**
+ * @author STEIMETZ Tangui <steimetz.tangui@ecole.ensicaen.fr>
+ * @version 1.0.2
+ * @date 22 janvier 2022
+ */
+
+#include "liste.h"
+
+/**
+ * @brief Create a Cell object
+ * 
+ * @param x : the new element 
+ * @return LISTE_CELL 
+ */
+static LISTE_CELL createCell ( listeElement x );
+/**
+ * @brief Destroy a Cell object
+ * 
+ * @param cell : the cell to destroy
+ * @return boolean 
+ */
+static boolean destroyCell ( LISTE_CELL cell );
+
+static LISTE_CELL createCell ( listeElement x)
+{
+    LISTE_CELL newCell;
+
+    newCell =  ( LISTE_CELL ) malloc ( sizeof ( _liste_cell ) );
+    newCell->contents[0] = x[0];
+    newCell->contents[1] = x[1];
+    newCell->followingCell = NULL;
+    return newCell;
+}
+
+static boolean destroyCell ( LISTE_CELL cell )
+{
+    if ( cell != NULL ) {
+        free ( cell );
+        cell = NULL;
+        return true;
+    }
+    return false;
+}
+
+LISTE createListe () 
+{
+    LISTE newListe;
+    newListe.head = NULL;
+    newListe.tail = NULL;
+    return newListe;
+}
+
+boolean isEmpty ( LISTE liste )
+{
+    if ( liste.head == NULL ) {
+        return true;
+    }
+    return false;
+}
+
+LISTE addElementLifo ( LISTE liste, listeElement x )
+{
+    LISTE_CELL newCell;
+
+    newCell = createCell ( x );
+    if ( isEmpty ( liste ) ) {
+        liste.head = newCell;
+        liste.tail = newCell;
+        return liste;
+    }
+
+    newCell->followingCell = liste.tail;
+    liste.tail = newCell;
+    return liste;
+}
+
+LISTE removeElementListeCoord ( LISTE liste, listeElement* result , coord removeCoord)
+{
+    LISTE_CELL previousHead;
+    LISTE_CELL current;
+    
+    if ( isEmpty ( liste ) ) {
+        return liste;
+    }
+    current = liste.head;
+    while (removeCoord != current->contents && current != NULL) {
+        previousHead = current;
+        current = current->followingCell;
+    }
+    if (current == NULL) {
+        return liste;
+    }
+    result[0][0] = current->contents[0]; 
+    result[0][1] = current->contents[1];
+    if (current == liste.head) {
+        liste.head = current->followingCell;
+    } else if ( current == liste.tail) {
+        liste.tail = previousHead;
+    } else {
+        previousHead->followingCell = current->followingCell;
+    }
+    destroyCell ( current );
+    return liste;
+}
+
+
+
+LISTE removeElementListe ( LISTE liste, listeElement* result )
+{
+    LISTE_CELL previousHead;
+    
+    if ( isEmpty ( liste ) ) {
+        return liste;
+    }
+    result[0][0] = liste.head->contents[0]; 
+    result[0][1] = liste.head->contents[1];
+    previousHead = liste.head;
+    liste.head = liste.head->followingCell;
+    destroyCell ( previousHead );
+    return liste;
+}
+
+
+void destroyListe ( LISTE liste )
+{
+    coord trash;
+    while ( !isEmpty ( liste ) ) {
+        liste = removeElementListe ( liste, &trash );
+    }
+}
